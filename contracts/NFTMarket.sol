4e66361e4d28ba225612140d3d87a23d94852916
nishant <nishantthesingh@gmail.com>
2021-11-26 14:34:45 +0530

    First Commit

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,176 @@
+// contracts/Market.sol
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+
+contract NFTMarket is ReentrancyGuard {
+  using Counters for Counters.Counter;
+  Counters.Counter private _itemIds;
+  Counters.Counter private _itemsSold;
+
+  address payable contractOwner;
+  uint256 listingPrice = 0.025 ether;
+
+  struct MarketItem {
+    uint itemId;
+    address nftContract;
+    uint256 tokenId;
+    address payable seller;
+    address payable owner;
+    uint256 price;
+    bool sold;
+  }
+
+  mapping(uint256 => MarketItem) private idToMarketItem;
+
+  event MarketItemCreated (
+    uint indexed itemId,
+    address indexed nftContract,
+    uint256 indexed tokenId,
+    address seller,
+    address owner,
+    uint256 price,
+    bool sold
+    );
+
+  //Creating a function to let the front end know about the Listing Price.
+  function getListingPrice() public view returns (uint256){
+    return listingPrice;
+  }
+
+  //Function to listing an NFT on the market place for sale:
+  function createMarketItem(
+    address nftContract,
+    uint256 tokenId,
+    uint256 price
+  ) public payable nonReentrant {
+    require(price > 0, "Price must be at least 1 wei");
+    require(msg.value == listingPrice, "Price must be equal to listing price");
+
+    _itemIds.increment();
+    uint256 itemId = _itemIds.current();
+
+    idToMarketItem[itemId] =  MarketItem(
+      itemId,
+      nftContract,
+      tokenId,
+      payable(msg.sender),
+      payable(address(0)),
+      price,
+      false
+    );
+
+    //Transfering the ownership of NFT Tolen to the marketplace address
+    IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+    //Emmiting the created item details for the frontEnd:
+    emit MarketItemCreated(
+      itemId,
+      nftContract,
+      tokenId,
+      msg.sender,
+      address(0),
+      price,
+      false
+    );
+  }
+
+  //function to creater a sale of the nft owned by the contract
+  function createMarketSale(
+    address nftContract,
+    uint256 itemId
+    ) public payable nonReentrant{
+    
+    uint price = idToMarketItem[itemId].price;
+    uint tokenId = idToMarketItem[itemId].tokenId;
+    require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+    idToMarketItem[itemId].seller.transfer(msg.value); 
+    //Transfering the NFT price to the seller
+
+    IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId); 
+    //Transferig the ownership of the contract to the buyer
+
+    idToMarketItem[itemId].owner = payable(msg.sender); 
+    //Listing the buyer as the owner of the NFT in our array data 
+
+    idToMarketItem[itemId].sold = true; 
+    //Setting the NFT Token status as sold in out array data
+
+    _itemsSold.increment(); 
+    //One more item sold by the contract, yay!
+
+    payable(contractOwner).transfer(listingPrice); 
+    //Transfering the commission to the implementer of the contract
+    }
+
+// Just Copied and Pasted, need to come back and make sense of this again  
+  function fetchMarketItems() public view returns (MarketItem[] memory){
+    uint itemCount =_itemIds.current();
+    uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+    uint currentIndex = 0;
+
+    MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+    for (uint i =0; i<itemCount; i++) {
+      if (idToMarketItem[i+1].owner == address(0)){
+        uint currentId = i + 1;
+        MarketItem storage currentItem = idToMarketItem[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+      }
+    } 
+    return items;
+  }
+  /* Returns only items that a user has purchased */
+  function fetchMyNFTs() public view returns (MarketItem[] memory) {
+    uint totalItemCount = _itemIds.current();
+    uint itemCount = 0;
+    uint currentIndex = 0;
+
+    for (uint i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].owner == msg.sender) {
+        itemCount += 1;
+      }
+    }
+
+    MarketItem[] memory items = new MarketItem[](itemCount);
+    for (uint i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].owner == msg.sender) {
+        uint currentId = i + 1;
+        MarketItem storage currentItem = idToMarketItem[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+      }
+    }
+    return items;
+  }
+
+  /* Returns only items a user has created */
+  function fetchItemsCreated() public view returns (MarketItem[] memory) {
+    uint totalItemCount = _itemIds.current();
+    uint itemCount = 0;
+    uint currentIndex = 0;
+
+    for (uint i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].seller == msg.sender) {
+        itemCount += 1;
+      }
+    }
+
+    MarketItem[] memory items = new MarketItem[](itemCount);
+    for (uint i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].seller == msg.sender) {
+        uint currentId = i + 1;
+        MarketItem storage currentItem = idToMarketItem[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+      }
+    }
+    return items;
+  }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+const projectID = "280003d3f4934500962d4921bad2da3e"
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: 'https://polygon-mumbai.infura.io/v3/${projectID}',
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: 'https://polygon-mainnet.infura.io/v3/${projectID}',
+      accounts: [privateKey]
+    }
+  }
+  solidity: "0.8.4",
+};
