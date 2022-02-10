76462ddc5ccca992d2027fc2e524bf4a6efdaa73
nishant <nishantthesingh@gmail.com>
2022-02-10 17:44:20 +0530

    commited

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
-
+//1
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
